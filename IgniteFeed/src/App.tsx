85d731d8d56f8dd0a8e6005bde8b79970d459498
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-03 21:02:33 -0300

    feat: improving the application.

--- IgniteFeed/src/App.tsx
+++ IgniteFeed/src/App.tsx
@@ -1,11 +1,11 @@
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
-import { Post } from './components/Post';
+import { Post, PostType } from './components/Post';
 
 import './global.css';
 import styles from './App.module.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -71,14 +71,7 @@ export function App() {
         <Sidebar />
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            );
+            return <Post key={post.id} post={post} />;
           })}
         </main>
       </div>
