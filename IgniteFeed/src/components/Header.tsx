b6b10f1aca2376f55af6e1042ae568104a4cedde
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-03-04 18:22:44 -0300

    feat: App name change and Header add h1.

--- IgniteFeed/src/components/Header.tsx
+++ IgniteFeed/src/components/Header.tsx
@@ -5,7 +5,10 @@ import igniteLogo from '../assets/ignite-logo.svg';
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt='Logotipo do Ignite' />
+      <div className={styles.container}>
+        <img src={igniteLogo} alt='Logotipo do Ignite' />
+        <h1>Ignite Feed</h1>
+      </div>
     </header>
   );
 }
